da37268914fb7a6a0a6e97738f8558295cd8acb1
沈杰(AMD-PC) <598759654@qq.com>
2025-07-20 11:38:59 +0800

    springboot

---
+++ ssm06-springboot-part/springboot04-druid/src/main/java/com/atguigu/pojo/User.java
@@ -0,0 +1,21 @@
+package com.atguigu.pojo;
+
+import lombok.Data;
+
+/**
+ * projectName: com.atguigu.pojo
+ *
+ * @author: 赵伟风
+ * description:
+ */
+
+@Data
+public class User {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String gender;
+    private String classes;
+}
