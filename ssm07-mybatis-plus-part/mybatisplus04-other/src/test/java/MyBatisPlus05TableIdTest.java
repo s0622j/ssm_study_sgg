b444d8cf62856b3cfc6c323bb448f33e89d2fb4f
沈杰(AMD-PC) <598759654@qq.com>
2025-07-21 00:42:37 +0800

    springboot-end

---
+++ ssm07-mybatis-plus-part/mybatisplus04-other/src/test/java/MyBatisPlus05TableIdTest.java
@@ -0,0 +1,31 @@
+import com.atguigu.MainApplication;
+import com.atguigu.mapper.UserMapper;
+import com.atguigu.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * projectName: com.atguigu.test
+ *
+ * @author: 赵伟风
+ * description:
+ */
+
+@SpringBootTest(classes = MainApplication.class)
+public class MyBatisPlus05TableIdTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void test1(){
+        User user = new User();
+        user.setName("驴蛋蛋");
+        user.setAge(20);
+        user.setEmail("xxxx@qq.com");
+        //主键不要赋值
+        //主键的策略： 雪花算法 -》 不重复的数字 long
+        userMapper.insert(user);
+    }
+}
